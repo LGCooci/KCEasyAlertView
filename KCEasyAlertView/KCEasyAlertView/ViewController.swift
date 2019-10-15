e1a9146d46cfe86c878baa7be1f28ff433baaef8
lgcooci <cooci_tz@163.com>
2019-10-15 21:54:41 +0800

    0.0.1

---
+++ KCEasyAlertView/KCEasyAlertView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KCEasyAlertView
+//
+//  Created by Cooci on 2019/10/15.
+//  Copyright © 2019 Cooci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
